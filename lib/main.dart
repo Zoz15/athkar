76e4c6ce1d465d58a097d32bc702eb6b36967651
Zoz <ziad3072006+github@gmail.com>
2024-11-06 17:39:13 +0200

    athkar

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:athkar/homeScreen/home_screen.dart';
+import 'package:athkar/var.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    height = MediaQuery.of(context).size.height;
+    width = MediaQuery.of(context).size.width;
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomeScreen(),
+    );
+  }
+}
